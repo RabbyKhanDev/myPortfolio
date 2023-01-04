c2a72857b18634bfce894193cadb695cb57f0f66
Rabby Khan <rabbikhan365@gmail.com>
2023-01-04 11:05:04 +0600

    Updated

--- Files/portfolio.js
+++ Files/portfolio.js
@@ -1,10 +1,12 @@
 // Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+const header = document.getElementById("myDIV");
+const btns = header.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button.
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    });
+};
